c075ff444a7e5472ad63245afbddf6d5cbedb10f
0giru <dusrlf13@gmail.com>
2020-12-08 17:41:48 +0900

    SingleLinkedList Header File

---
+++ SingleLinkedList.h
@@ -0,0 +1,22 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int ElementType;
+
+typedef struct _Node{
+    int Data;
+    struct _Node* nextNode;
+}Node;
+
+Node* SLL_CreateNode(ElementType NewData);
+void SLL_DestroyNode(Node* Node);
+void SLL_AppendNode(Node** Head, Node* NewNode);
+void SLL_InsertNode(Node* Current, Node* NewNode);
+Node* SLL_GetNodeAt(Node* Head, int Location);
+void RemoveNode(Node** Head, Node* Remove);
+int SLL_GetNodeCount(Node* Head);
+
+#endif 
